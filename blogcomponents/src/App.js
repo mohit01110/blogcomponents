c6191b77b18ef8bab04dd2643751d35747260c3f
Mohit Choudhary <132933699+mohit01110@users.noreply.github.com>
2024-02-05 17:11:48 +0530

    Add files via upload

---
+++ blogcomponents/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './component/LoginForm';
+import Registration from './component/Registration';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <LoginForm/>
+     <Registration/>
+    </div>
+    
+  );
+}
+
+export default App;
